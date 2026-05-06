87321b68db491c9b8eb59d64e35f008a486701d6
oneteammk-commits <oneteam.mk@gmail.com>
2026-05-06 17:11:51 +0900

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,14 @@
 import { RegistrationForm } from './components/RegistrationForm'
+import { ViewPage } from './components/ViewPage'
 
 function App() {
+  const path = window.location.pathname
+  const viewMatch = path.match(/^\/view\/(.+)$/)
+
+  if (viewMatch) {
+    return <ViewPage id={viewMatch[1]} />
+  }
+
   return <RegistrationForm />
 }
 

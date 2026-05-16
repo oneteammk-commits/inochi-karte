0ee31f10950f26b62c5f53ba38e6254bfb9b1cdf
oneteammk-commits <oneteam.mk@gmail.com>
2026-05-16 09:44:47 +0900

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
 import { RegistrationForm } from './components/RegistrationForm'
 import { ViewPage } from './components/ViewPage'
+import { EditPage } from './components/EditPage'
 
 function App() {
   const path = window.location.pathname
   const viewMatch = path.match(/^\/card\/(.+)$/)
+  const editMatch = path.match(/^\/edit\/(.+)$/)
 
+  if (editMatch) {
+    return <EditPage id={editMatch[1]} />
+  }
   if (viewMatch) {
     return <ViewPage id={viewMatch[1]} />
   }
-
   return <RegistrationForm />
 }
 
